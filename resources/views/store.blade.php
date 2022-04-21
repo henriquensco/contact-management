1d70cea84e065d339328ce037ef2633f17f4dc99
henriquensco <nascimentocostahenrique@gmail.com>
2022-04-21 17:09:20 -0300

    Changes the page title

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Contact Management - Home</title>
+    <title>Contact Management - New Contact</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- Fonts -->
--- resources/views/update.blade.php
+++ resources/views/update.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Contact Management - Home</title>
+    <title>Contact Management - Update</title>
     @stack('styles')
 
     <!-- Fonts -->
